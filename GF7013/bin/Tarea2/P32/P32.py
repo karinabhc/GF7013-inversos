a490e5dd4cc9048fd096c3f273aa18909871211f
Teresa Peralta <70674489+tereperalta@users.noreply.github.com>
2025-06-20 01:48:27 -0400

    actualiza P32 en repositorio y estado P44 (sin terminar)

--- GF7013/bin/Tarea2/P32/P32.py
+++ GF7013/bin/Tarea2/P32/P32.py
@@ -1,15 +1,16 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-from GF7013.bin.Tarea2.P1.datos import obtener_datos_elipses
+
 
 # importar modulos relevantes del paquete GF7013
-pythonpackagesfolder = '../../../..' # no modificar si no se mueve de carpeta este notebook
+pythonpackagesfolder = '../../../../' # no modificar si no se mueve de carpeta este notebook
 import sys
 sys.path.append(pythonpackagesfolder)
 
 # modelo directo de la recta
 #from GF7013.models.ajuste_ortogonal_recta import recta
+from GF7013.bin.Tarea2.P1.datos import obtener_datos_elipses
 from GF7013.models.ajuste_ortogonal_recta.forward import forward
 from GF7013.probability_functions.pdf.pdf_uniform_nD import pdf_uniform_nD  # Para la distribución a priori
 from GF7013.probability_functions.likelihood.likelihood_function import likelihood_function
@@ -40,9 +41,6 @@ x_obs, y_obs, sigma_x, sigma_y = obtener_datos_elipses(
 
 
 
-normas = np.sqrt(x_obs**2 + y_obs**2)
-max_norma = np.max(normas)
-
 a_min = -15
 a_max = 15
 theta_min = -360
@@ -57,7 +55,6 @@ par = {
 }
 
 fprior = pdf_uniform_nD(par)
-print(fprior.draw())
 
 ############## Creo que de aqui pa bajo ojito
 modelo_forward = forward(x_obs=x_obs, y_obs=y_obs, sigma_x=sigma_x, sigma_y=sigma_y)
@@ -69,8 +66,6 @@ n_obs = len(x_obs)
 
 
 # Parámetros teóricos
-#mu = np.array([-1.0, 4.0])
-#cov = np.array([[2.0, 1.0], [1.0, 4.0]])
 par = {'mu': np.zeros(n_obs), 'cov': np.eye(n_obs)}
 pdf = pdf_normal(par)
 
@@ -79,27 +74,28 @@ likelihood_fun = likelihood_function(forward=modelo_forward, pdf_data=pdf)
 
 # Definir la distribución de propuesta (normal multivariada)
 # Matriz de covarianza para la propuesta
-#sigma_a = 0.5  # desviación estándar para el parámetro 'a'
-#sigma_theta = 0.5  # desviación estándar para el parámetro 'theta'
-#cov_matrix = np.array([[sigma_a**2, 0],0000 
-#                       [0, sigma_theta**2]])
+sigma_a = 0.5  # desviación estándar para el parámetro 'a'
+sigma_theta = 0.5  # desviación estándar para el parámetro 'theta'
+cov_matrix = np.array([[sigma_a**2, 0], 
+                       [0, sigma_theta**2]])
 
-alpha=1/100
-delta = (fprior.par['upper_lim'] - fprior.par['lower_lim']) 
-cov_matrix = np.diag((alpha*delta))  # 2D array!!
-#deltas =fprior.par['upper_lim'] - fprior.par['lower_lim']
-#cov_matrix = np.diag(alpha * deltas**2)  # Matriz de cov
+
+
 
 proposal = proposal_normal(cov=cov_matrix)
 
 
 # Parámetros del Metropolis
-NumSamples = int(1e5)
-NumBurnIn = int(0.1 * NumSamples)
+NumSamples = int(5e4)
+NumBurnIn = int(0.3 * NumSamples)
+#NumBurnIn = 0
+
 use_log_likelihood = True
 
 #m0 = np.array([0.0, 0.0])  # Modelo inicial, valores iniciales para [a, theta]
 m0 = fprior.draw()
+print('m0:')
+print(m0)
 
 cadena = metropolis(m0=m0, 
                     likelihood_fun=likelihood_fun, 
@@ -111,32 +107,54 @@ cadena = metropolis(m0=m0,
                     save_samples=True,
                     beta=1)
 
-#fpost =_fpost(fm_prior= fprior, fm_like=likelihood_fun, beta=1)
-#log_fpost =_log_fpost(fm_prior= fprior, fm_like=likelihood_fun, beta=1)
-
 # Extraer muestras
 samples = cadena['samples']
 a_samples = samples.m_set[:, 0]
 theta_samples = samples.m_set[:, 1]
 
 # --- Graficar evolución de parámetros ---
-fig, axs = plt.subplots(2, 1, figsize=(10, 6), sharex=True)
-axs[0].scatter(np.arange(len(a_samples)), a_samples, alpha=0.7)
-axs[0].set_ylabel('a')
-axs[0].grid(True)
-axs[0].set_title('Evolución del parámetro a')
+fig = plt.figure(figsize=(10, 6))
+ax = fig.add_subplot(111)
 
-axs[1].scatter(np.arange(len(a_samples)),theta_samples, alpha=0.7, color='orange')
-axs[1].set_ylabel('theta [grados]')
-axs[1].set_xlabel('Iteración')
-axs[1].grid(True)
+sc=ax.scatter(a_samples,theta_samples,alpha=0.7,c=np.arange(len(a_samples)),cmap='rainbow')
+plt.colorbar(sc, ax=ax, label='Índice de Muestra')
+ax.set_xlabel('a')
+ax.set_ylabel('theta [grados]')
+ax.grid(True)
 
-axs[1].set_title('Evolución del parámetro theta')
-
-plt.suptitle("Evolución de la cadena de Metropolis")
+plt.suptitle("Evolución de la cadena de Metropolis (30% Burn-in)", fontsize=16)
 plt.tight_layout()
 plt.show()
-    
 
+# --- Graficar histogramas de parámetros ---
+fig = plt.figure(figsize=(10, 8))
+gs = gridspec.GridSpec(2, 2, width_ratios=[4, 1], height_ratios=[1, 4],
+                       wspace=0.05, hspace=0.05)
+ax_join = fig.add_subplot(gs[1, 0])
+counts, xedges, yedges, im = ax_join.hist2d(a_samples, theta_samples, bins=50, cmap='viridis')
+fig.colorbar(im, ax=ax_join, label='Cuentas')
+ax_join.set_title('Histograma 2D (PDF conjunta)')
+ax_join.set_xlabel('a')
+ax_join.set_ylabel('theta [grados]')
 
+# Marginal para a
+ax_a = fig.add_subplot(gs[0, 0], sharex=ax_join)
+a_hist, a_bins, _ = ax_a.hist(a_samples, bins=100, density=True, alpha=0.6, color='blue', label='Histograma de a')
+ax_a.set_title('Marginal para a')
+ax_a.set_ylabel('Densidad')
+ax_a.legend()
+ax_a.tick_params(labelbottom=False)
+pos = ax_a.get_position()
+ax_a.set_position([pos.x0, pos.y0 + 0.03, pos.width, pos.height])
+
+# Marginal para theta
+ax_theta = fig.add_subplot(gs[1, 1], sharey=ax_join)
+theta_hist, theta_bins, _ = ax_theta.hist(theta_samples, bins=100, density=True, alpha=0.6, color='green', orientation='horizontal', label='Histograma de theta')
+ax_theta.set_title('Marginal para theta')
+ax_theta.set_xlabel('Densidad')
+ax_theta.legend()
+ax_theta.tick_params(labelleft=False)   
+fig.suptitle("Distribución de Parámetros: Histograma Conjunto y Marginales", fontsize=14, y=0.965)
+plt.tight_layout(rect=[0, 0, 1, 0.95])
+plt.show()
 
--- GF7013/bin/Tarea2/P44/P44.py
+++ GF7013/bin/Tarea2/P44/P44.py
@@ -1,22 +1,28 @@
+"""
+Script similar a P32, pero con modelos iniciales generados como muestras de fprior
+"""
 import matplotlib.pyplot as plt
 import numpy as np
 
-from GF7013.bin.Tarea2.P1.datos import obtener_datos_elipses
+
 
 # importar modulos relevantes del paquete GF7013
-pythonpackagesfolder = '../../../..' # no modificar si no se mueve de carpeta este notebook
+pythonpackagesfolder = '../../../../' # no modificar si no se mueve de carpeta este notebook
 import sys
 sys.path.append(pythonpackagesfolder)
 
 # modelo directo de la recta
 #from GF7013.models.ajuste_ortogonal_recta import recta
+from GF7013.bin.Tarea2.P1.datos import obtener_datos_elipses
 from GF7013.models.ajuste_ortogonal_recta.forward import forward
 from GF7013.probability_functions.pdf.pdf_uniform_nD import pdf_uniform_nD  # Para la distribución a priori
 from GF7013.probability_functions.likelihood.likelihood_function import likelihood_function
 from GF7013.probability_functions.pdf.pdf_normal import pdf_normal  # Para la distribución de los residuos
 from GF7013.sampling.metropolis.proposal_normal import proposal_normal
-from GF7013.sampling.metropolis.metropolis import metropolis, _fpost,_log_fpost
+from GF7013.sampling.metropolis_in_parallel import metropolis_in_parallel_POOL, metropolis_in_parallel_SERIAL
 import matplotlib.gridspec as gridspec
+
+from GF7013.model_parameters import ensemble
 
 
 N = 50
@@ -37,4 +43,130 @@ x_obs, y_obs, sigma_x, sigma_y = obtener_datos_elipses(
                                         deltay = delta_y,
                                         sigma_x = desviacion_estandar_x,
                                         sigma_y = desviacion_estandar_y)
+
+
+
+a_min = -15
+a_max = 15
+theta_min = -360
+theta_max = 360 
+
+lower_lim = np.array([a_min, theta_min])
+upper_lim = np.array([a_max, theta_max])
+
+par = {
+    'lower_lim': lower_lim,
+    'upper_lim': upper_lim
+}
+
+fprior = pdf_uniform_nD(par)
+
+############## Creo que de aqui pa bajo ojito
+modelo_forward = forward(x_obs=x_obs, y_obs=y_obs, sigma_x=sigma_x, sigma_y=sigma_y)
+
+# Crear la distribución de probabilidad para los datos (residuos normalizados)
+# Como forward.eval() devuelve residuos normalizados, usamos una normal estándar
+# El número de parámetros debe coincidir con el número de observaciones
+n_obs = len(x_obs)
+
+
+# Parámetros teóricos
+par = {'mu': np.zeros(n_obs), 'cov': np.eye(n_obs)}
+pdf = pdf_normal(par)
+
+# Crear la función de verosimilitud usando tu clase del paquete
+likelihood_fun = likelihood_function(forward=modelo_forward, pdf_data=pdf)
+
+# Definir la distribución de propuesta (normal multivariada)
+# Matriz de covarianza para la propuesta
+sigma_a = 0.5  # desviación estándar para el parámetro 'a'
+sigma_theta = 0.5  # desviación estándar para el parámetro 'theta'
+cov_matrix = np.array([[sigma_a**2, 0], 
+                       [0, sigma_theta**2]])
+
+
+
+
+proposal = proposal_normal(cov=cov_matrix)
+
+
+# Parámetros del Metropolis
+NumSamples = int(5e4)
+NumBurnIn = int(0.3 * NumSamples)
+#NumBurnIn = 0
+
+use_log_likelihood = True
+
+#m0 = np.array([0.0, 0.0])  # Modelo inicial, valores iniciales para [a, theta]
+m0 = ensemble(
+              Npar = 2, Nmodels=NumSamples, 
+              use_log_likelihood=use_log_likelihood,
+              beta=1
+            ) # fprior.draw()
+
+# generamos los modelos iniciales como muestras de fprior
+m0.m_set[:] = fprior.draw()
+print('m0:')
+print(m0.m_set)
+
+m, aceptance_ratios = metropolis_in_parallel_POOL(m0=m0, 
+                    likelihood_fun=likelihood_fun, 
+                    pdf_prior=fprior, 
+                    proposal=proposal, 
+                    num_samples=NumSamples,
+                    num_burnin=NumBurnIn,
+                    use_log_likelihood=use_log_likelihood,
+                    save_samples=True,
+                    beta=1)
+
+# Extraer muestras
+samples = cadena['samples']
+a_samples = samples.m_set[:, 0]
+theta_samples = samples.m_set[:, 1]
+
+# --- Graficar evolución de parámetros ---
+fig = plt.figure(figsize=(10, 6))
+ax = fig.add_subplot(111)
+
+sc=ax.scatter(a_samples,theta_samples,alpha=0.7,c=np.arange(len(a_samples)),cmap='rainbow')
+plt.colorbar(sc, ax=ax, label='Índice de Muestra')
+ax.set_xlabel('a')
+ax.set_ylabel('theta [grados]')
+ax.grid(True)
+
+plt.suptitle("Evolución de la cadena de Metropolis (30% Burn-in)", fontsize=16)
+plt.tight_layout()
+plt.show()
+
+# --- Graficar histogramas de parámetros ---
+fig = plt.figure(figsize=(10, 8))
+gs = gridspec.GridSpec(2, 2, width_ratios=[4, 1], height_ratios=[1, 4],
+                       wspace=0.05, hspace=0.05)
+ax_join = fig.add_subplot(gs[1, 0])
+counts, xedges, yedges, im = ax_join.hist2d(a_samples, theta_samples, bins=50, cmap='viridis')
+fig.colorbar(im, ax=ax_join, label='Cuentas')
+ax_join.set_title('Histograma 2D (PDF conjunta)')
+ax_join.set_xlabel('a')
+ax_join.set_ylabel('theta [grados]')
+
+# Marginal para a
+ax_a = fig.add_subplot(gs[0, 0], sharex=ax_join)
+a_hist, a_bins, _ = ax_a.hist(a_samples, bins=100, density=True, alpha=0.6, color='blue', label='Histograma de a')
+ax_a.set_title('Marginal para a')
+ax_a.set_ylabel('Densidad')
+ax_a.legend()
+ax_a.tick_params(labelbottom=False)
+pos = ax_a.get_position()
+ax_a.set_position([pos.x0, pos.y0 + 0.03, pos.width, pos.height])
+
+# Marginal para theta
+ax_theta = fig.add_subplot(gs[1, 1], sharey=ax_join)
+theta_hist, theta_bins, _ = ax_theta.hist(theta_samples, bins=100, density=True, alpha=0.6, color='green', orientation='horizontal', label='Histograma de theta')
+ax_theta.set_title('Marginal para theta')
+ax_theta.set_xlabel('Densidad')
+ax_theta.legend()
+ax_theta.tick_params(labelleft=False)   
+fig.suptitle("Distribución de Parámetros: Histograma Conjunto y Marginales", fontsize=14, y=0.965)
+plt.tight_layout(rect=[0, 0, 1, 0.95])
+plt.show()
 
