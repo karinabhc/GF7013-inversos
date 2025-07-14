1a8fc3f9c57f7dec3f436238170ca13e4c760dc9
tereperalta <teresaperalta@ug.uchile.cl>
2025-07-14 19:07:12 -0400

    agrega variables de ambiente faltantes en test_metropolis_paralelo_POOL

--- GF7013/bin/Tarea2/tests/test_metropolis_paralelo_POOL.py
+++ GF7013/bin/Tarea2/tests/test_metropolis_paralelo_POOL.py
@@ -1,5 +1,13 @@
 #! /usr/bin/env python
 import sys, os
+os.environ["OMP_NUM_THREADS"] = "1"
+os.environ["MKL_NUM_THREADS"] = "1"
+os.environ["MKL_DOMAIN_NUM_THREADS"] = "1"
+os.environ["OPENBLAS_NUM_THREADS"] = "1"
+os.environ["VECLIB_MAXIMUM_THREADS"] = "1"
+os.environ["NUMEXPR_NUM_THREADS"] = "1"
+
+
 # get folder of this module
 this_module_folder = os.path.dirname(os.path.abspath(__file__))
 # add GF7013 package to PYTHONPATH
